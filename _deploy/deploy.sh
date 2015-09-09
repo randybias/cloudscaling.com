9fa748a438a9236d13697fdafd4ebd2791e127f2
hamza <hamza@flickerbox.com>
2015-09-09 15:26:30 -0700

    Update deploy script, add s3_website config for stage and production Bug: 34219

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-confirm () {
-    # call with a prompt string or use a default
+# Check if the environment is provided
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+# Confirm function to avoid mistakes
+confirm() {
     read -p "$1 ([y]es or [N]o): "
     case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
         y|yes) echo 1 ;;
@@ -9,20 +16,44 @@ confirm () {
     esac
 }
 
-echo "Building blog"
-jekyll build
-echo "Dry run"
-s3_website push --dry-run
+# Deploy to production
+deploy_prod() {
+	echo "Building blog"
+	jekyll build --config _config.yml
+	echo "Dry run"
+	s3_website push --dry-run --config-dir _s3_prod_config
 
-## Get Confirmation
+	if [ $(confirm) -eq 1 ]
+	then
+	  echo "Deploying blog to s3 cloudscaling.com"
+	  s3_website push --config-dir _s3_prod_config
+	else
+	  echo "Stopped Deployment" 
+	fi
+}
+
+# Deploy to stage
+deploy_stage() {
+	echo "Building blog"
+	jekyll build --config _config_dev.yml
+	echo "Dry run"
+	s3_website push --dry-run --config-dir _s3_stage_config
+
+	if [ $(confirm) -eq 1 ]
+	then
+	  echo "Deploying blog to s3 stage.www.cloudscaling.com"
+	  s3_website push --config-dir _s3_stage_config
+	else
+	  echo "Stopped Deployment" 
+	fi
+}
 
 
-if [ $(confirm) -eq 1 ]
-then
-  echo "Deploying blog to s3"
-  s3_website push
+if [ $1 = "production" ]; then
+   deploy_prod
+elif [ $1 = "stage" ]; then
+	deploy_stage
 else
-  echo "Stopped Deployment" 
+  echo "Invalid enviromennt, use 'production' or 'stage'"
+  exit 1
 fi
-
-
