fcef73a086db0980fa623d9708ed92b427112afc
hamza <hamza@flickerbox.com>
2015-09-10 10:23:18 -0700

    Update config file and deploy script Bug: 34219

--- _deploy/deploy.sh
+++ _deploy/deploy.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
+red='\033[0;31m'
+green='\033[0;32m'
+nc='\033[0m' # No Color
 
 # Check if the environment is provided
 if [ $# -eq 0 ]
   then
-    echo "No arguments supplied"
+    echo ${red}"No arguments supplied"
     exit 1
 fi
 
 # Confirm function to avoid mistakes
 confirm() {
-    read -p "$1 ([y]es or [N]o): "
+    read -p "Are you sure? ([y]es or [N]o): "
     case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
         y|yes) echo 1 ;;
         *)     echo 0 ;;
@@ -18,42 +21,44 @@ confirm() {
 
 # Deploy to production
 deploy_prod() {
-	echo "Building blog"
+	echo ${green}"Building blog for production"${nc}
 	jekyll build --config _config_prod.yml
-	echo "Dry run"
+
+	echo ${red}"Dry run"${nc}
 	s3_website push --dry-run --config-dir _s3_prod_config
 
 	if [ $(confirm) -eq 1 ]
 	then
-	  echo "Deploying blog to s3 cloudscaling.com"
+	  echo ${green}"Deploying blog to S3 cloudscaling.com"${nc}
 	  s3_website push --config-dir _s3_prod_config
 	else
-	  echo "Stopped Deployment" 
+	  echo ${green}"Stopped Deployment"${nc}
 	fi
 }
 
 # Deploy to stage
 deploy_stage() {
-	echo "Building blog"
+	echo ${green}"Building blog for staging"${nc}
 	jekyll build --config _config_stage.yml
-	echo "Dry run"
+
+	echo ${red}"Dry run"${nc}
 	s3_website push --dry-run --config-dir _s3_stage_config
 
 	if [ $(confirm) -eq 1 ]
 	then
-	  echo "Deploying blog to s3 stage.www.cloudscaling.com"
+	  echo ${green}"Deploying blog to S3 stage.www.cloudscaling.com"${nc}
 	  s3_website push --config-dir _s3_stage_config
 	else
-	  echo "Stopped Deployment" 
+	  echo ${green}"Stopped Deployment"${nc} 
 	fi
 }
 
-
+# Take user input to call build for production or staging
 if [ $1 = "production" ]; then
    deploy_prod
 elif [ $1 = "stage" ]; then
 	deploy_stage
 else
-  echo "Invalid enviromennt, use 'production' or 'stage'"
+  echo ${red}"Invalid environment, use 'production' or 'stage'"${nc}
   exit 1
 fi
