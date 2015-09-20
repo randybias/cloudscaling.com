a631f24921a7fb40fbf44f8fcbfe3aaf9f48e59f
Hamza Essaoui <hamza@flickerbox.com>
2015-09-20 09:38:34 -0700

    add search, single page author, authors page , data authors, loader

--- js/master.js
+++ js/master.js
@@ -1,19 +1,55 @@
+$(window).load(function() {
+	// $(".loader").fadeOut("slow");
+})
+
 $(function() {
 	$( ".search-icon" ).on( "click", function() {
-		console.log( $( this ).text() );
+		$('#global-search').submit();
 	});
 
-    $.fn.footnotify = function() {
-        // Get the content
-        var str = this.html();
-        // Set the regex string
-        var regex = /(\[[0-9]{1}\])/ig
-        // Replace plain text links by hyperlinks
-        var replaced_text = str.replace(regex, "<a name='footnotes-$1'><span class='footnotes'><sup>$1</sup></span></a>");
-        // Echo link
-        this.html(replaced_text);
-        return this;
-    }
- 
-    // $( ".post-content" ).footnotify();
+	$.fn.footnotify = function() {
+
+		var str = this.html();
+
+		var regex = /(\[[0-9]{1}\])/ig
+
+		var count = 0;
+
+
+		replaced_text = str.replace(regex, function(match, contents, offset, s, count)
+		{
+			return "<a href='#footnotes-"+match+"' name='footnotes-"+match+"' data-offset='"+ offset +"' data-match="+ match +"><span class='footnotes'><sup>"+match+"</sup></span></a>";
+		}
+		); 
+
+		this.html(replaced_text);
+		return this;
+	}
+
+	if ($( ".post-content" ).length )
+		$( ".post-content" ).footnotify();
+});
+var getUrlParameter = function getUrlParameter(sParam) {
+	var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+	sURLVariables = sPageURL.split('&'),
+	sParameterName,
+	i;
+
+	for (i = 0; i < sURLVariables.length; i++) {
+		sParameterName = sURLVariables[i].split('=');
+
+		if (sParameterName[0] === sParam) {
+			return sParameterName[1] === undefined ? true : sParameterName[1];
+		}
+	}
+};
+$( document ).ready(function() {
+
+	// if (window.location.href.indexOf("search.html?s=") > -1) {
+	// 	var search_query = getUrlParameter('s');
+	// 	$('.nav-search-input').val(search_query);
+	// 	window.data.then(function(loaded_data) {
+	// 		$('#submit_search').click();
+	// 	});
+	// }
 });
--- js/search.js
+++ js/search.js
@@ -1,87 +1,91 @@
+
 jQuery(function() {
-  // Initalize lunr with the fields it will be searching on. I've given title
-  // a boost of 10 to indicate matches on this field are more important.
+  
   window.idx = lunr(function () {
     this.field('id');
     this.field('title', { boost: 10 });
     this.field('author');
+    this.field('date');
+    this.field('author_url');
     this.field('category');
-    this.field('excerpt');
+    this.field('content');
   });
 
-  // Download the data from the JSON file we generated
+
   window.data = $.getJSON('/search_data.json');
 
-  // Wait for the data to load and add it to lunr
+
   window.data.then(function(loaded_data){
     $.each(loaded_data, function(index, value){
       window.idx.add(
         $.extend({ "id": index }, value)
-      );
+        );
     });
   });
 
-  //setup before functions
-    var typingTimer;                //timer identifier
-    var doneTypingInterval = 500;  //time in ms, 5 second for example
-    var $input = $('#site_search');
 
-    //on keyup, start the countdown
-    $input.on('keyup', function () {
-      clearTimeout(typingTimer);
-      typingTimer = setTimeout(doneTyping, doneTypingInterval);
-    });
+  var typingTimer;                
+  var doneTypingInterval = 500;  
+  var $input = $('.nav-search-input');
 
-    //on keydown, clear the countdown 
-    $input.on('keydown', function () {
-      clearTimeout(typingTimer);
-    });
 
-    //user is "finished typing," do something
-    function doneTyping () {
-      // event.preventDefault();
-      var query = $("#search_box").val(); // Get the value for the text field
-      var results = window.idx.search(query); // Get lunr to perform a search
-            console.log(results);
+  $input.on('keyup', function () {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(doneTyping, doneTypingInterval);
+  });
 
-      display_search_results(results); // Hand
-    }
 
-  // Event when the form is submitted
+  $input.on('keydown', function () {
+    clearTimeout(typingTimer);
+  });
+
+
+  function doneTyping () {
+
+    var query = $(".nav-search-input").val(); 
+    var results = window.idx.search(query); 
+
+    display_search_results(results); 
+  }
+
   $("#site_search").submit(function(){
-      event.preventDefault();
-      var query = $("#search_box").val(); // Get the value for the text field
-      var results = window.idx.search(query); // Get lunr to perform a search
-            console.log(results);
+    event.preventDefault();
+    var query = $(".nav-search-input").val(); 
+    var results = window.idx.search(query); 
 
-      display_search_results(results); // Hand the results off to be displayed
+    display_search_results(results); 
   });
+
+  if (window.location.href.indexOf("search.html?s=") > -1) {
+    var search_query = getUrlParameter('s');
+    $('.nav-search-input').val(search_query);
+    window.data.then(function(loaded_data) {
+      $(".loader").fadeOut("slow");
+      $('#submit_search').click();
+    });
+  }
 
   function display_search_results(results) {
 
     var $search_results = $("#results");
 
-    // Wait for data to load
     window.data.then(function(loaded_data) {
+      $(".loader").fadeOut("slow");
 
-      // Are there any results?
       if (results.length) {
-        $search_results.empty(); // Clear any old results
-  	      console.log("result > 1 => "+results.length);
+        $search_results.empty(); 
 
-        // Iterate over the results
+
         results.forEach(function(result) {
           var item = loaded_data[result.ref];
 
-          // Build a snippet of HTML for this result
-          var appendString = '<li><a href="' + item.url + '">' + item.title + '</a></li>';
+          var appendString = '<div class="index--single-post"> <div class="post-header"> <h1 class="post-title"><a href="' + item.url + '">' + item.title + '</a></h1> <p class="post-meta">Posted on <span class="date-meta">' + item.date + '</span> by <span class="author-meta"><a href="' + item.author_url + '"> ' + item.author + ' </a></span> </p> </div> <div class="content"> <div class="excerpt"> ' + item.content + ' </div>   </div> </div>';
 
-          // Add it to the results
+
           $search_results.append(appendString);
         });
       } else {
-      	 console.log("result should be 0 => "+results.length);
-        $search_results.html('<li>No results found</li>');
+        $search_results.html('<p class="center">No results found<p>');
       }
     });
   }
